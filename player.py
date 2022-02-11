e1a111e4747e8ae9c88be42ff5bf9bb02ffefcdb
natefrost <bloodbursting@gmail.com>
2022-02-11 23:12:57 +0100

    playing capabilities for the players

--- player.py
+++ player.py
@@ -1,5 +1,24 @@
+"""
+Implementation of player objects
+
+"""
+
+import numpy as np
+
 class Player:
     def __init__(self, id, card):
         self.wins = 0
-        self._card = card
+        self.card1 = card
+        self.card2 = 0
         self._id = id
+
+    def play_card1(self):
+        temp_card = self.card1
+        self.card1 = 0
+        return temp_card  
+
+    def play_card2(self):
+        temp_card = self.card2
+        self.card2 = 0
+        return temp_card   
+    
