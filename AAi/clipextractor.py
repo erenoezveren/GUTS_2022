b9e327aeba2c51a4b1a2dc21d36a2f1da9fca9ec
natefrost <bloodbursting@gmail.com>
2022-02-12 22:33:46 +0100

    fixed bug + limited to 5 clips

--- AAi/clipextractor.py
+++ AAi/clipextractor.py
@@ -34,32 +34,35 @@ for post in lsfres.json()['data']['children']:
 #URLS now contains links to the top clips from Livestream Fails, which we can use twitchdownloader.py to download clips from:
 
 #run os command to download clips:
+counter=0
 
 for index, clip in enumerate(URLS):
+    if counter<5:
+        client_id = 'Your Client ID'
+        basepath = 'tmp/'
+        slug = clip.rpartition('/')[-1]
+        clip_info = requests.get("https://api.twitch.tv/kraken/clips/" + slug, headers={"Client-ID": 'gp762nuuoqcoxypju8c569th9wz7q5', "Accept":"application/vnd.twitchtv.v5+json"}).json()
 
-    client_id = 'Your Client ID'
-    basepath = 'tmp/'
-    slug = clip.rpartition('/')[-1]
-
-    clip_info = requests.get("https://api.twitch.tv/kraken/clips/" + slug, headers={"Client-ID": 'gp762nuuoqcoxypju8c569th9wz7q5', "Accept":"application/vnd.twitchtv.v5+json"}).json()
-
-    print(clip_info)
-    thumb_url = clip_info['thumbnails']['medium']
-    mp4_url = thumb_url.split("-preview",1)[0] + ".mp4"
-    out_filename = "clip" + str(index+1) + ".mp4"
-    output_path = (basepath + out_filename)
+        try:
+            thumb_url = clip_info['thumbnails']['medium']
+            mp4_url = thumb_url.split("-preview",1)[0] + ".mp4"
+            out_filename = "clip" + str(index) + ".mp4"
+            output_path = (basepath + out_filename)
 
 
-    def dl_progress(count, block_size, total_size):
-        percent = int(count * block_size * 100 / total_size)
-        sys.stdout.write("\r...%d%%" % percent)
-        sys.stdout.flush()
+            def dl_progress(count, block_size, total_size):
+                percent = int(count * block_size * 100 / total_size)
+                sys.stdout.write("\r...%d%%" % percent)
+                sys.stdout.flush()
 
-    # create the basepath directory
-    if not os.path.exists(basepath):
-        os.makedirs(basepath)
+            # create the basepath directory
+            if not os.path.exists(basepath):
+                os.makedirs(basepath)
 
-    try:
-        urllib.request.urlretrieve(mp4_url, output_path, reporthook=dl_progress)
-    except:
-        print("An exception occurred")
+            try:
+                urllib.request.urlretrieve(mp4_url, output_path, reporthook=dl_progress)
+            except:
+                print("An exception occurred")
+            counter+=1                
+        except:
+            pass
