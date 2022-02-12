23ca57f5396880159e57b2a5d692912803c7b8f9
natefrost <bloodbursting@gmail.com>
2022-02-12 06:42:26 +0100

    AssemblyAI project

---
+++ AAi/AssemblyAIproject.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import requests
+import urllib.request
+import argparse
+import requests
+import pprint
+import json
+import moviepy.editor as mp
+from time import sleep
+
+auth_key = 'c61f276a614a4416a8e9287b02812296'
+
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
+
+headers_auth_only = {'authorization': auth_key}
+
+headers = {
+    "authorization": auth_key,
+    "content-type": "application/json"}
+
+def read_file(filename):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(5242880)
+            if not data:
+                break
+            yield data
+arr=os.listdir('tmp/')
+
+for i in range(len(arr)):
+    clip = mp.VideoFileClip(r'tmp/'+arr[i])
+    clip.audio.write_audiofile(r"mp/clip"+str(i)+".mp3")
+
+
+ar = os.listdir('mp/')
+for clip in ar:
+    upload_response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file('mp/'+clip))
+    audio_url = upload_response.json()["upload_url"]
+
+    transcript_request = {'audio_url': audio_url}
+    transcript_response = requests.post("https://api.assemblyai.com/v2/transcript", json=transcript_request, headers=headers)
+    _id = transcript_response.json()["id"]
+
+    polling_response = requests.get("https://api.assemblyai.com/v2/transcript/" + _id, headers=headers)
+    print(polling_response.json()['status'])
+
+    filename = 'txt/'+transcript_response.json()['id'] + '.txt'
+    while polling_response.json()['status'] != 'completed':
+        sleep(30)
+        polling_response = requests.get(transcript_endpoint+"/"+transcript_response.json()['id'], headers=headers)
+        print("File is", polling_response.json()['status'])
+    with open(filename, 'w') as f:
+        f.write(polling_response.json()['text'])
+        print('Transcript saved to', filename)
---
+++ AAi/clipextractor.py
@@ -0,0 +1,65 @@
+
+"""
+Python script which downloads the top clips from livestreamfails using the Twitch and Reddit API keys
+
+"""
+
+import os
+import sys
+import requests
+import urllib.request
+import argparse
+
+auth = requests.auth.HTTPBasicAuth('BLuYjxErXU1iGIooZq1hfw', 'xpWGDzNAzXmT3TzYa9o79w1lEMgXlA')
+data = {'grant_type': 'password',
+        'username': 'natefrostmd',
+        'password': 'guts2022password'}
+
+headers = {'User-Agent': 'app by u/natefrostmd'}
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+TOKEN = res.json()['access_token']
+headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+lsfres = requests.get("https://oauth.reddit.com/r/livestreamfails/hot",
+                   headers=headers)
+
+URLS = []
+
+for post in lsfres.json()['data']['children']:
+    url = post['data']['url']
+    if url.startswith("https://clips.twitch.tv/"):
+        URLS.append(url)
+
+#URLS now contains links to the top clips from Livestream Fails, which we can use twitchdownloader.py to download clips from:
+
+#run os command to download clips:
+
+for clip in URLS:
+
+    client_id = 'Your Client ID'
+    basepath = 'tmp/'
+    slug = clip.rpartition('/')[-1]
+
+    clip_info = requests.get("https://api.twitch.tv/kraken/clips/" + slug, headers={"Client-ID": 'gp762nuuoqcoxypju8c569th9wz7q5', "Accept":"application/vnd.twitchtv.v5+json"}).json()
+
+    print(clip_info)
+    thumb_url = clip_info['thumbnails']['medium']
+    mp4_url = thumb_url.split("-preview",1)[0] + ".mp4"
+    out_filename = slug + ".mp4"
+    output_path = (basepath + out_filename)
+
+
+    def dl_progress(count, block_size, total_size):
+        percent = int(count * block_size * 100 / total_size)
+        sys.stdout.write("\r...%d%%" % percent)
+        sys.stdout.flush()
+
+    # create the basepath directory
+    if not os.path.exists(basepath):
+        os.makedirs(basepath)
+
+    try:
+        urllib.request.urlretrieve(mp4_url, output_path, reporthook=dl_progress)
+    except:
+        print("An exception occurred")
